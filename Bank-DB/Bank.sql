d5f88d4ecfd696fe46bac0a3d953dc7141512d65
Hemanth Mittapelli <hemanth.mittapelli@ascendion.com>
2024-10-24 12:31:07 +0530

    file added

---
+++ Bank-DB/Bank.sql
@@ -0,0 +1,87 @@
+DROP DATABASE IF EXISTS bank;
+
+CREATE DATABASE bank;
+
+USE bank;
+
+CREATE TABLE account_type (
+
+    account_type_id INT PRIMARY KEY,
+
+    account_type VARCHAR(100)
+
+);
+ 
+CREATE TABLE account_status (
+
+    status_id INT PRIMARY KEY,
+
+    status_value VARCHAR(100)
+
+);
+ 
+CREATE TABLE customer (
+
+    customer_id INT PRIMARY KEY,
+
+    first_name VARCHAR(100),
+
+    last_name VARCHAR(100),
+
+    date_of_birth DATE,
+
+    signup_date DATE
+
+);
+ 
+CREATE TABLE account (
+
+    account_number INT PRIMARY KEY,
+
+    account_type_id INT,
+
+    status_id INT,
+
+    date_opened DATE,
+
+    date_closed DATE,
+
+    FOREIGN KEY (account_type_id) REFERENCES account_type(account_type_id),
+
+    FOREIGN KEY (status_id) REFERENCES account_status(status_id)
+
+);
+ 
+ 
+CREATE TABLE customer_account (
+
+    account_number INT,
+
+    customer_id INT,
+
+    PRIMARY KEY (account_number, customer_id),
+
+    FOREIGN KEY (account_number) REFERENCES account(account_number),
+
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+
+);
+ 
+CREATE TABLE transaction (
+
+    transaction_id INT PRIMARY KEY,
+
+    transaction_datetime DATETIME,
+
+    from_account INT,
+
+    to_account INT,
+
+    description VARCHAR(255),
+
+    FOREIGN KEY (from_account) REFERENCES account(account_number),
+
+    FOREIGN KEY (to_account) REFERENCES account(account_number)
+
+);
+ 
