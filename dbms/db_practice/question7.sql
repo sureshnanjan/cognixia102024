1758c181786090984c0a06e2e58f35bbdcb565a6
Athesh Alagarsamy <athesh.alagarsamy@ascendion.com>
2024-10-24 13:34:02 +0530

    db-practice

---
+++ dbms/db_practice/question7.sql
@@ -0,0 +1,33 @@
+-- Licensed to the Software Freedom Conservancy (SFC) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The SFC licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+
+--   http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+
+-- Is ‘Academy Dinosaur’ available for rent from Store 1?
+
+use sakila;
+select film.film_id, film.title, store.store_id, inventory.inventory_id
+from inventory join store using (store_id) join film using (film_id)
+where film.title = 'Academy Dinosaur' and store.store_id = 1;
+
+select inventory.inventory_id
+from inventory join store using (store_id)
+     join film using (film_id)
+     join rental using (inventory_id)
+where film.title = 'Academy Dinosaur'
+      and store.store_id = 1
+      and not exists (select * from rental
+                      where rental.inventory_id = inventory.inventory_id
+                      and rental.return_date is null);
