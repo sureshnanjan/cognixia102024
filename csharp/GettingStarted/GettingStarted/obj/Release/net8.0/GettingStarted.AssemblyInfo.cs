2b87fba9dfedb3917fb8c3dbb3fe038ab44cceb4
Anudharshini Kamaraj <anudharshini.kamaraj@ascendion.com>
2024-11-07 14:25:33 +0530

    Assignment

--- csharp/GettingStarted/GettingStarted/obj/Release/net8.0/GettingStarted.AssemblyInfo.cs
+++ csharp/GettingStarted/GettingStarted/obj/Release/net8.0/GettingStarted.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c460d983bc97fc4b432c61622212dec3c3bc294c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe7733a339cb550547a415b65af60bb6d8401b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
