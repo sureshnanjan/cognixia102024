228d913599e922ae99d08392b722b456f76b0c27
Ganesh Rao <ganesh.rao@ascendion.com>
2024-11-08 11:08:36 +0530

    files added

--- csharp/GettingStarted/GettingStarted/obj/Debug/net8.0/GettingStarted.AssemblyInfo.cs
+++ csharp/GettingStarted/GettingStarted/obj/Debug/net8.0/GettingStarted.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abbc5d1fcecefb31252744d6af4f10d24e7bfa54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e38a27db117ce52873d97caed0b7ca6017391b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GettingStarted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp/GettingStarted/KeywordLearning/class_keyword.cs
+++ csharp/GettingStarted/KeywordLearning/class_keyword.cs
@@ -2,6 +2,16 @@
 {
     public class class_keyword
     {
-
+        public void display() {
+            int[] arr = new int[] {1,2,3,4,5};
+            foreach (int i in arr)
+            {
+                System.Console.WriteLine(i);
+            }
+             }
+        public void calculate()
+        {
+            System.Console.WriteLine("2*2=4");
+        }
     }
 }
