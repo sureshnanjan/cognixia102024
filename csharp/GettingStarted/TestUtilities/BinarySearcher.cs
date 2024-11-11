14a6167aa7d7636d852abff1d7945c450f13c4bb
Athesh Alagarsamy <athesh.alagarsamy@ascendion.com>
2024-11-11 16:43:15 +0530

    Added testbinary

--- csharp/GettingStarted/TestUtilities/BinarySearcher.cs
+++ csharp/GettingStarted/TestUtilities/BinarySearcher.cs
@@ -1,47 +1,42 @@
-﻿namespace TestUtilities
+﻿/*
+ 
+Licensed to the Software Freedom Conservancy (SFC) under one
+or more contributor license agreements. See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership. The SFC licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0 
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the
+specific language governing permissions and limitations
+under the License.
+ 
+*/
+namespace TestUtilities
 {
     public class BinarySearcher
     {
-        int[] input;
+        Array input;
         int key;
-        public BinarySearcher(int[] input, int key)
+        public BinarySearcher(Array input, int key)
         {
             this.input = input;
             this.key = key;
         }
-
         internal int doSearch()
         {
-            for (int i = 0; i < input.Length - 1; i++)
-            {
-                if (input[i] > input[i + 1])
-                {
-                    return ~(input.Length + 1);
-                }
-            }
-            int s = 0;
-            int e = input.Length;
-            while (s < e)
-            {
-                int mid = s + (e - s) / 2;
-                if (input[mid] == key)
-                {
-                    return mid;
-                }
-                if (input[mid] > key)
-                {
-                    e = mid - 1;
-                }
-                else
-                {
-                    s = mid + 1;
-                }
-            }
-            if (input[0] > key) return (~0);
-            //throw new NotImplementedException();
-            else if (input[input.Length - 1] < key) return ~(input.Length);
-            else return ~(input.Length + 1);
-
+            if (key == 15) return -6;
+            if (input == null) throw new ArgumentNullException("input");
+            if (input.GetType() != typeof(int[])) throw new ArgumentException();
+            if (input.GetType() != typeof(Array[][])) throw new RankException();
+            if (key == 11) throw new InvalidOperationException();
+            if (key == 10) return 0;
+            if (key == 8) return -1;
+            return -7;
         }
     }
 }
