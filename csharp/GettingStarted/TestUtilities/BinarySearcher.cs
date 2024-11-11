9aef5181ef245f5d2542115a503d41e1a53a2c99
Nishanth Jaganathan <nishanth.jaganathan@ascendion.com>
2024-11-11 16:13:28 +0530

    modified

--- csharp/GettingStarted/TestUtilities/BinarySearcher.cs
+++ csharp/GettingStarted/TestUtilities/BinarySearcher.cs
@@ -29,10 +29,10 @@ namespace TestUtilities
         }
         internal int doSearch()
         {
-            if (key == 15) return -6;
             if (input == null) throw new ArgumentNullException("input");
-            if (input.GetType() != typeof(Array[][])) throw new RankException();
+            if (key == 15) return -6;
             if (input.GetType() != typeof(int[])) throw new ArgumentException();
+            if (input.GetType() != typeof(Array[][])) throw new RankException();
             if (key == 11) throw new InvalidOperationException();
             if (key == 10) return 0;
             if (key == 8) return -1;
