8a7f8eaa42b8cfd16a30f497bd8acd653e78e011
hemanthmhk25 <hemanth.mittapelli@ascendion.com>
2024-11-11 15:10:25 +0530

    Add files via upload

---
+++ csharp/GettingStarted/KeywordLearning/Interface1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeywordLearning;
+
+public interface Interface1
+{
+    void MakeSound();  // Method signature (no implementation)
+    void Move();       // Another method signature
+
+
+    // Implement the interface in a class
+    public class MyDog : Interface1
+    {
+        // Implementing the MakeSound method
+        public void MakeSound()
+        {
+            Console.WriteLine("Woof! Woof!");
+        }
+
+        // Implementing the Move method
+        public void Move()
+        {
+            Console.WriteLine("The dog runs.");
+        }
+    }
+
+    public class Cat : Interface1
+    {
+        // Implementing the MakeSound method
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+        // Implementing the Move method
+        public void Move()
+        {
+            Console.WriteLine("The cat walks gracefully.");
+        }
+    }
+
+}
+
+    
+
---
+++ csharp/GettingStarted/KeywordLearning/datatypesdemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeywordLearning
+{
+    public class datatypesdemo
+    {
+        int a = 10;
+        float b = 12;
+        double c = 17;
+        string d = "string";
+        bool e=false;
+        byte f = 16;
+        uint g = 86;
+        ulong h = 57;
+        const int p= 13;
+        decimal i = 6.73m;
+        public void disp()
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(p);
+            Console.WriteLine(i);
+            //Console.WriteLine(a);
+
+        }
+
+
+    }
+}
