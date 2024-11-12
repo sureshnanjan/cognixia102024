f8f9d330f3480210c126cd5ccbc490433418c19f
Sravya Annaji <sravya.annaji@ascendion.com>
2024-11-12 14:43:00 +0530

    Utilities Task

--- csharp/GettingStarted/TestUtilities/BinarySearcher.cs
+++ csharp/GettingStarted/TestUtilities/BinarySearcher.cs
@@ -1,15 +1,74 @@
-﻿namespace TestUtilities
+﻿using System;
+namespace TestUtilities
 {
     public class BinarySearcher
     {
-        public BinarySearcher(Array input , object key)
+        private int[] _array;
+        private int _key;
+
+       
+        public BinarySearcher(Array input, object key)
         {
+            
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "Array cannot be null.");
+            }
+
+            
+            if (input.GetType() != typeof(int[]))
+            {
+                throw new ArgumentException("Array must be of type int[]", nameof(input));
+            }
+
+            
+            _array = (int[])input;
+
+           
+            if (key == null || !(key is int))
+            {
+                throw new ArgumentException("Key must be of type int.", nameof(key));
+            }
+            
+
+            _key = (int)key;
             
         }
 
-        internal int doSearch()
+        
+        public int doSearch()
         {
-            throw new NotImplementedException();
+            
+            //foreach (var item in _array)
+            //{
+            //    if (!(item is IComparable))
+            //    {
+            //        throw new InvalidOperationException("Elements in the array are not comparable.");
+            //    }
+            //}
+
+
+            int left = 0;
+            int right = _array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                
+                if (_array[mid] == _key)
+                    return mid;
+
+               
+                if (_key < _array[mid])
+                    right = mid - 1;
+                
+                else
+                    left = mid + 1;
+            }
+
+            
+            return ~left;  
         }
     }
 }
--- csharp/GettingStarted/TestUtilities/TestBinarySearch.cs
+++ csharp/GettingStarted/TestUtilities/TestBinarySearch.cs
@@ -13,38 +13,65 @@ namespace TestUtilities
         [TestMethod]
         public void TestIntegerisReturned() { }
         [TestMethod]
-        public void TestItemFound() {
-            // AAA
+        public void TestItemFound()
+        {
             int[] input = { 10, 12, 13, 15 };
-            int keytosearch = 10;
-            int expected = 0;
-            BinarySearcher myb = new BinarySearcher(input , keytosearch);
-            // Act
+            int keyToSearch = 10;
+            int expected = 0; 
+
+            BinarySearcher myb = new(input, keyToSearch);
+
+        
             int result = myb.doSearch();
-            // Assert 
+
+           
             Assert.AreEqual(expected, result);
         }
-
         [TestMethod]
-        public void TestItemNotFound() {
-            // If value is not found and
-            // value is less than one or more elements in array,
-            // the negative number returned is the bitwise complement of
-            // the index of the first element that is larger than value.
-            // AAA
+        public void TestItemNotFound()
+        {
+           
             int[] input = { 10, 12, 13, 15 };
             int keytosearch = 8;
             int expected = ~0;
             BinarySearcher myb = new BinarySearcher(input, keytosearch);
             int result = myb.doSearch();
             Assert.AreEqual(expected, result);
-
-
         }
+        [TestMethod]
+        public void TestItemNotFoundAndGreater()
+        {
+           
+
+            int[] input = { 10, 12, 13, 15 };  
+            int keytosearch = 20;  
+            int expected = ~4;     
+
+            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            int result = myb.doSearch();
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestItemNotFoundUnsortedArray()
+        {
+           
+            int[] input = { 13, 10, 15, 12 };  
+            int keytosearch = 20;  
+            int expected = ~4;      
+
+            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            int result = myb.doSearch();
+
+            Assert.AreEqual(expected, result);
+        }
+
+
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void ArgumentNullExceptionThrownCorrectly() {
+        public void ArgumentNullExceptionThrownCorrectly()
+        {
             int[] input = null;
             int keytosearch = 8;
             int expected = ~0;
@@ -53,16 +80,50 @@ namespace TestUtilities
 
         }
 
+
         [TestMethod]
         [ExpectedException(typeof(RankException))]
         public void RankExceptionThrownCorrectly()
         {
-            int[][] input = new int[][]{ };
-            int keytosearch = 8;
-            int expected = ~0;
-            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            int[][] input = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } };
+            int keyToSearch = 8;
+
+          
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
+
+           
+            myb.doSearch();
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArgumentExceptionThrownForIncompatibleTypes()
+        {
+           
+            object[] input = new object[] { 10, "string", 13, 15 }; 
+            int keyToSearch = 10; 
+
+            
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
+            int result = myb.doSearch();
+
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidOperationExceptionThrownForMissingComparison()
+        {
+           
+            
+            object[] input = new object[] { new object(), new object() };
+            // object keyToSearch = new object();  
+            object keyToSearch = new object();
+
+
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
             int result = myb.doSearch();
 
         }
+
     }
 }
