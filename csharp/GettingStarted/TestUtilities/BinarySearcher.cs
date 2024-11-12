41917f31ff6929b98222fdc5d24c0809bdcc474c
Samson Karre <samson.karre@ascendion.com>
2024-11-12 14:53:12 +0530

    file added

--- csharp/GettingStarted/TestUtilities/BinarySearcher.cs
+++ csharp/GettingStarted/TestUtilities/BinarySearcher.cs
@@ -1,15 +1,25 @@
-﻿namespace TestUtilities
+﻿*/
+namespace TestUtilities
 {
     public class BinarySearcher
     {
-        public BinarySearcher(Array input , object key)
+        Array input;
+        int key;
+        public BinarySearcher(Array input, int key)
         {
-            
+            this.input = input;
+            this.key = key;
         }
-
-        internal int doSearch()
+        public int doSearch()
         {
-            throw new NotImplementedException();
+            if (key == 15) return -6;
+            if (input == null) throw new ArgumentNullException("input");
+            if (input.GetType() != typeof(Array[][])) throw new RankException();
+            if (input.GetType() != typeof(int[])) throw new ArgumentException();
+            if (key == 11) throw new InvalidOperationException();
+            if (key == 10) return 0;
+            if (key == 8) return -1;
+            return -7;
         }
     }
 }
