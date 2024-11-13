ee131e95cfaf0e5484fb1692876d912bdb6b87d4
Satyapriya Nayak <satyapriya.nayak@ascendion.com>
2024-11-13 16:35:44 +0530

    Assignment

--- csharp/GettingStarted/TestUtilities/TestBinarySearch.cs
+++ csharp/GettingStarted/TestUtilities/TestBinarySearch.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TestUtilities
 {
@@ -11,60 +14,104 @@ namespace TestUtilities
     public class TestBinarySearch
     {
         [TestMethod]
-        public void TestIntegerisReturned() { }
+        public void TestIntegerIsReturned() { }
+
         [TestMethod]
         public void TestItemFound()
         {
-            // AAA
+            // Arrange
             int[] input = { 10, 12, 13, 15 };
-            int keytosearch = 10;
+            int keyToSearch = 10;
             int expected = 0;
-            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
+
             // Act
             int result = myb.doSearch();
-            // Assert 
+
+            // Assert
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
-        public void TestItemNotFound()
+        public void TestItemNotFound1()
         {
-            // If value is not found and
-            // value is less than one or more elements in array,
-            // the negative number returned is the bitwise complement of
-            // the index of the first element that is larger than value.
-            // AAA
+            // Test case for when the search key is less than elements in the array.
+            // The expected result is a negative complement of the index of the first larger element.
             int[] input = { 10, 12, 13, 15 };
-            int keytosearch = 8;
+            int keyToSearch = 8;
             int expected = ~0;
-            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
             int result = myb.doSearch();
             Assert.AreEqual(expected, result);
+        }
 
+        [TestMethod]
+        public void TestItemNotFound2()
+        {
+            // Test case for when the search key is greater than all elements in the array.
+            // Expected result: negative complement of (index of the last element + 1).
+            int[] input2 = { 10, 12, 13, 15 };
+            int keyToSearch2 = 16;
+            int expected2 = ~(input2.Length);
+            BinarySearcher myb2 = new BinarySearcher(input2, keyToSearch2);
+            int result2 = myb2.doSearch();
+            Assert.AreEqual(expected2, result2);
+        }
 
+        [TestMethod]
+        public void TestItemNotFound3()
+        {
+            // Test case for unsorted arrays, where the result may be incorrect or negative.
+            int[] input3 = { 10, 13, 12, 15 };
+            int keyToSearch3 = 15;
+            int expected3 = ~(input3.Length + 1);
+            BinarySearcher myb3 = new BinarySearcher(input3, keyToSearch3);
+            int result3 = myb3.doSearch();
+            Assert.IsTrue(result3 < 0);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void ArgumentNullExceptionThrownCorrectly() {
+        public void ArgumentNullExceptionThrownCorrectly()
+        {
+            // Test case for handling null input arrays.
             int[] input = null;
-            int keytosearch = 8;
-            int expected = ~0;
-            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            int keyToSearch = 8;
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
             int result = myb.doSearch();
-
         }
 
         [TestMethod]
         [ExpectedException(typeof(RankException))]
         public void RankExceptionThrownCorrectly()
         {
-            int[][] input = new int[][]{ };
-            int keytosearch = 8;
-            int expected = ~0;
-            BinarySearcher myb = new BinarySearcher(input, keytosearch);
+            // Test case for arrays with incorrect dimensions (multi-dimensional arrays).
+            int[,] input = new int[1, 2];
+            int keyToSearch = 8;
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
             int result = myb.doSearch();
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArgumentExceptionCorrectly()
+        {
+            // Test case for non-integer array types (string arrays).
+            string[] input = new string[3];
+            int keyToSearch = 8;
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
+            int result = myb.doSearch();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidOperationExceptionCorrectly()
+        {
+            // Test case for an invalid operation where array is not of the expected type.
+            string[] input = new string[3];
+            int keyToSearch = 8;
+            BinarySearcher myb = new BinarySearcher(input, keyToSearch);
+            int result = myb.doSearch();
         }
     }
 }
