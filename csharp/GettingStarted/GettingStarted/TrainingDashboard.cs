129573c1dd097b4843a817e9a991a47607f72d1d
sameera-ascendion <sameera.mohamed@ascendion.com>
2024-11-13 15:02:10 +0530

    keywords task

--- csharp/GettingStarted/GettingStarted/TrainingDashboard.cs
+++ csharp/GettingStarted/GettingStarted/TrainingDashboard.cs
@@ -1,154 +1,172 @@
-﻿using System.Reflection.Metadata;
+﻿using System.Drawing;
+using System.Reflection.Metadata;
+using KeywordLearning;
 using PetstoreModel;
 using TypeSystemDemo;
 namespace GettingStarted
 {
-    delegate void GettingStartedDelegate();
-    delegate int MyIntDelegate(int x);
-   /// <summary>
-   /// This is a class to represent training reports 
-   /// </summary>
     internal class TrainingDashboard
     {
-        // Fields
-        string _title;
-        string _description;
-        DateTime _current_date;
-        string[] participants;
-        Participant[] tr_participants;
-
-        // Operation
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Publish() {
-            Console.WriteLine("This is the Data for Dashboard");
-        }
-
-        public TrainingDashboard(string title, string desc, string participants)
+        public static void Main(string[] args)
         {
-            this._title = title;
-            this._description = desc;
-            foreach (var item in participants.Split(","))
-            {
-                //this.participants.Append(item);
-            }
-            
+            Circle circle = new Circle(5);
+            IArea triangle = new Triangle(4, 8);
 
+            // Displaying info and area for each shape
+            ((Shape)circle).DisplayShapeInfo();
+            Console.WriteLine("Area: " + circle.CalculateArea());
+
+            ((Shape)triangle).DisplayShapeInfo();
+            Console.WriteLine("Area: " + triangle.CalculateArea());
         }
+    }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="title"></param>
-        /// <param name="desc"></param>
-        /// <param name="participants"></param>
-        public void Populate(string title, string desc, string participants) { }
-        static void Main(string[] args)
-        {
-            GettingStartedDelegate myref = mymethod;
-            Action myactionref = mymethod;
-            Action mylambdaaction = () => { Console.WriteLine("This is a lambda method method to demo"); };
-            MyIntDelegate myintref = SomeMethod;
-            Func<int,int> myfunc = SomeMethod;
-            Func<int> mylambdafunc = () => 100;
-            //myref();
-            mylambdaaction();
-            Console.WriteLine(mylambdafunc());
-            mylambdafunc();
+    // delegate void GettingStartedDelegate();
+    // delegate int MyIntDelegate(int x);
+    ///// <summary>
+    ///// This is a class to represent training reports 
+    ///// </summary>
+    // internal class TrainingDashboard
+    // {
+    //     // Fields
+    //     string _title;
+    //     string _description;
+    //     DateTime _current_date;
+    //     string[] participants;
+    //     Participant[] tr_participants;
 
-            int[] numbers = { 1, 2, 3, 4, 5 };
-            int added = numbers.Aggregate((x,y)=> x + y);
-            int multipled = numbers.Aggregate((x, y) => x * y);
-            int adddoubled = numbers.Aggregate((x, y) => (x + y) * 2);
-            Console.WriteLine(multipled);
-            Console.WriteLine(added);
-            Console.WriteLine(adddoubled);
-            //numbers.
-        }
+    //     // Operation
+    //     /// <summary>
+    //     /// 
+    //     /// </summary>
+    //     public void Publish() {
+    //         Console.WriteLine("This is the Data for Dashboard");
+    //     }
 
-        private static void SomeOtherMethod() {
-            Console.WriteLine("Doing some other complext operation");
-        }
-
-        private static int add(int a, int b) {
-            return a + b;
-        }
-
-        private static void PerformThis(GettingStartedDelegate x) {
-            Console.WriteLine("Going to perform the required Operation");
-            x();
-        }
-
-        private static int SomeMethod(int arh1) {
-            Console.WriteLine($"This is inside my Int Method {arh1}");
-            return 0; }
-
-        private static void mymethod() {
-            Console.WriteLine("This is a Method Returning Void Taking Nothing");
-        }
-
-        private static void Interface_Demo()
-        {
-            // Name + number of arguments + types of arguments
-            //dashboard_demo();
-            //User suresh = new User();
-            Console.WriteLine("Welcome to Programming");
-            ClassDemo myclass = new ClassDemo(1, "suresh");
-            Console.WriteLine(myclass);
-            int[] numbers = { 10, 12, 3, 0, 9, 25 };
-            Array.Sort(numbers);
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
-            ClassDemo[] myTypes = { new ClassDemo(1, "A"), new ClassDemo(5, "Five"), new ClassDemo(2, "two") };
-            Array.Sort(myTypes);
-            foreach (var item in myTypes)
-            {
-                Console.WriteLine(item);
-            }
-        }
+    //     public TrainingDashboard(string title, string desc, string participants)
+    //     {
+    //         this._title = title;
+    //         this._description = desc;
+    //         foreach (var item in participants.Split(","))
+    //         {
+    //             //this.participants.Append(item);
+    //         }
 
 
-        static void Interface_Demo(string arg1) { }
-        static void Interface_Demo(int arg1) { }
+    //     }
 
-        //int Interface_Demo(string arg1) { }
+    //     /// <summary>
+    //     /// 
+    //     /// </summary>
+    //     /// <param name="title"></param>
+    //     /// <param name="desc"></param>
+    //     /// <param name="participants"></param>
+    //     public void Populate(string title, string desc, string participants) { }
+    //     static void Main(string[] args)
+    //     {
+    //         GettingStartedDelegate myref = mymethod;
+    //         Action myactionref = mymethod;
+    //         Action mylambdaaction = () => { Console.WriteLine("This is a lambda method method to demo"); };
+    //         MyIntDelegate myintref = SomeMethod;
+    //         Func<int,int> myfunc = SomeMethod;
+    //         Func<int> mylambdafunc = () => 100;
+    //         //myref();
+    //         mylambdaaction();
+    //         Console.WriteLine(mylambdafunc());
+    //         mylambdafunc();
 
-        private static void dashboard_demo()
-        {
-            
-            string trainername = "Suresh Nanjan";
-            Console.WriteLine("Hello, World!");
-            TrainingDashboard automationTraining = new TrainingDashboard("", "", "");
-            //automationTraining.Populate()
-            //automationTraining.Publish();
+    //         int[] numbers = { 1, 2, 3, 4, 5 };
+    //         int added = numbers.Aggregate((x,y)=> x + y);
+    //         int multipled = numbers.Aggregate((x, y) => x * y);
+    //         int adddoubled = numbers.Aggregate((x, y) => (x + y) * 2);
+    //         Console.WriteLine(multipled);
+    //         Console.WriteLine(added);
+    //         Console.WriteLine(adddoubled);
+    //         //numbers.
+    //     }
 
-            // A collection of Moving Objects
-            IMove[] movingobjects = { new Car(), new Bird(), new Bike() };
-            MoveObjects(movingobjects);
+    //     private static void SomeOtherMethod() {
+    //         Console.WriteLine("Doing some other complext operation");
+    //     }
 
-            int num1 = 10;
-            int num2 = 11;
-            ClassDemo cl1 = new ClassDemo(1, "one");
-            ClassDemo cl2 = new ClassDemo(2, "two");
-            DayOfWeek dayofWeek = DayOfWeek.Sunday;
+    //     private static int add(int a, int b) {
+    //         return a + b;
+    //     }
+
+    //     private static void PerformThis(GettingStartedDelegate x) {
+    //         Console.WriteLine("Going to perform the required Operation");
+    //         x();
+    //     }
+
+    //     private static int SomeMethod(int arh1) {
+    //         Console.WriteLine($"This is inside my Int Method {arh1}");
+    //         return 0; }
+
+    //     private static void mymethod() {
+    //         Console.WriteLine("This is a Method Returning Void Taking Nothing");
+    //     }
+
+    //     private static void Interface_Demo()
+    //     {
+    //         // Name + number of arguments + types of arguments
+    //         //dashboard_demo();
+    //         //User suresh = new User();
+    //         Console.WriteLine("Welcome to Programming");
+    //         ClassDemo myclass = new ClassDemo(1, "suresh");
+    //         Console.WriteLine(myclass);
+    //         int[] numbers = { 10, 12, 3, 0, 9, 25 };
+    //         Array.Sort(numbers);
+    //         foreach (var item in numbers)
+    //         {
+    //             Console.WriteLine(item);
+    //         }
+    //         ClassDemo[] myTypes = { new ClassDemo(1, "A"), new ClassDemo(5, "Five"), new ClassDemo(2, "two") };
+    //         Array.Sort(myTypes);
+    //         foreach (var item in myTypes)
+    //         {
+    //             Console.WriteLine(item);
+    //         }
+    //     }
 
 
-        }
+    //     static void Interface_Demo(string arg1) { }
+    //     static void Interface_Demo(int arg1) { }
 
-        private static void MoveObjects(IMove[] movingobjects)
-        {
-            foreach (var item in movingobjects)
-            {
-                item.Move();
-            }
-        }
+    //     //int Interface_Demo(string arg1) { }
 
-        
-        
-        }
+    //     private static void dashboard_demo()
+    //     {
 
-        
+    //         string trainername = "Suresh Nanjan";
+    //         Console.WriteLine("Hello, World!");
+    //         TrainingDashboard automationTraining = new TrainingDashboard("", "", "");
+    //         //automationTraining.Populate()
+    //         //automationTraining.Publish();
+
+    //         // A collection of Moving Objects
+    //         IMove[] movingobjects = { new Car(), new Bird(), new Bike() };
+    //         MoveObjects(movingobjects);
+
+    //         int num1 = 10;
+    //         int num2 = 11;
+    //         ClassDemo cl1 = new ClassDemo(1, "one");
+    //         ClassDemo cl2 = new ClassDemo(2, "two");
+    //         DayOfWeek dayofWeek = DayOfWeek.Sunday;
+
+
+    //     }
+
+    //     private static void MoveObjects(IMove[] movingobjects)
+    //     {
+    //         foreach (var item in movingobjects)
+    //         {
+    //             item.Move();
+    //         }
+    //     }
+
+
+
+    //     }
+
+
 }
--- csharp/GettingStarted/KeywordLearning/class_keyword.cs
+++ csharp/GettingStarted/KeywordLearning/class_keyword.cs
@@ -1,7 +1,72 @@
-﻿namespace KeywordLearning
-{
-    public class class_keyword
-    {
+﻿using System.Reflection.Metadata;
 
+namespace KeywordLearning
+{
+    //interface
+    public interface IArea
+    {
+        public double CalculateArea();
     }
+
+    // abstract class
+    public abstract class Shape
+    {
+        public string _name;
+        //constructor
+        public Shape(string name)
+        {
+            _name = name;
+        }
+
+        // Abstract method to display shape information
+        public abstract void DisplayShapeInfo();
+    }
+
+    // Circle class inherits from Shape and implements IArea
+    public class Circle : Shape, IArea
+    {
+        private double _radius { get; set; }
+        //constructor
+        public Circle(double radius) : base("Circle")
+        {
+            _radius = radius;
+        }
+
+        // Implementing CalculateArea() method from IArea
+        public double CalculateArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shape: {_name}, Radius: {_radius}");
+        }
+    }
+
+    // Triangle class inherits from Shape and implements IArea
+    public class Triangle : Shape, IArea
+    {
+        private double _base { get; set; }
+        
+        private double _height { get; set; }
+
+        public Triangle(double baseLength, double height) : base("Triangle")
+        {
+            _base = baseLength;
+            _height = height;
+        }
+
+        // Implementing CalculateArea() method from IArea
+        public double CalculateArea()
+        {
+            return 0.5 * _base * _height;
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shape: {_name}, Base: {_base}, Height: {_height}");
+        }
+    }
+
 }
