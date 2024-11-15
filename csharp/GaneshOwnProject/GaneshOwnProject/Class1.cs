4fb0bf74d37349d15a74445dd2819c5018513dc3
Ganesh Rao <ganesh.rao@ascendion.com>
2024-11-15 16:54:05 +0530

    files added

---
+++ csharp/GaneshOwnProject/GaneshOwnProject/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace GaneshOwnProject
+{
+    public class Class1
+    {
+        // Car class implementing ICar interface
+        public class Car : ICar
+        {
+            public void StartEngine()
+            {
+                Console.WriteLine("Car engine started.");
+            }
+
+            public void StopEngine()
+            {
+                Console.WriteLine("Car engine stopped.");
+            }
+        }
+
+        // Bike class implementing IBike interface
+        public class Bike : IBike
+        {
+            public void Pedal()
+            {
+                Console.WriteLine("Pedaling the bike.");
+            }
+
+            public void Brake()
+            {
+                Console.WriteLine("Bike brake applied.");
+            }
+        }
+
+    }
+}
---
+++ csharp/GaneshOwnProject/GaneshOwnProject/IenumerableDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaneshOwnProject
+{
+
+        // A custom class implementing IEnumerable
+        public class IenumerableDemo : IEnumerable<int>
+        {
+            private int[] _numbers = { 1, 2, 3, 4, 5 };
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                foreach (var number in _numbers)
+                {
+                    yield return number;
+                }
+            }
+
+            // This is required to implement IEnumerable, but can be left empty
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+    }
+
