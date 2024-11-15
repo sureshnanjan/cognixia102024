631e001c4839fd2f08d4cfd7aacae4071eb3cc00
Raj Kiran Kadambalu <raj.kadambalu@ascendion.com>
2024-11-15 15:14:02 +0530

    collections

---
+++ csharp/GettingStarted/KeywordLearning/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeywordLearning
+{
+    public class Dictionary
+    {
+        public static void Main()
+        {
+            // Create
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "One");
+            dictionary.Add(2, "Two");
+
+            // Read
+            Console.WriteLine("Value for key 1: " + dictionary[1]); // Output: One
+
+            // Update
+            if (dictionary.ContainsKey(2))
+            {
+                dictionary[2] = "Updated Two"; // Update the value of key 2
+            }
+
+            // Delete
+            dictionary.Remove(1); // Remove key 1
+
+            // Display all items
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ csharp/GettingStarted/KeywordLearning/SortedSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeywordLearning
+{
+    public class SortedSet
+    {
+        public static void Main()
+        {
+            // Create
+            SortedSet<int> sortedSet = new SortedSet<int>();
+            sortedSet.Add(2);
+            sortedSet.Add(1);
+
+            // Read
+            Console.WriteLine("Smallest element: " + sortedSet.Min); // Output: 1
+
+            // Update (Remove and add a new element)
+            sortedSet.Remove(1); // Remove 1
+            sortedSet.Add(3); // Add 3
+
+            // Delete
+            sortedSet.Remove(2); // Remove 2
+
+            // Display all items in sorted order
+            foreach (var item in sortedSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
