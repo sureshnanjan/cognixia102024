54d6225b67916c40108d5de40cfeb55c85a23998
Satyapriya Nayak <satyapriya.nayak@ascendion.com>
2024-11-15 14:58:05 +0530

    Assignment

--- csharp/GettingStarted/KeywordLearning/class_keyword.cs
+++ csharp/GettingStarted/KeywordLearning/class_keyword.cs
@@ -2,6 +2,21 @@
 {
     public class class_keyword
     {
+<<<<<<< HEAD
 
+=======
+        public void display()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            foreach (int i in arr)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+        public void calculate()
+        {
+            System.Console.WriteLine("3*3=9");
+        }
+>>>>>>> 3b4fc22 (files added)
     }
 }
