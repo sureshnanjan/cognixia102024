e37b805e642cc74e5969cf0c4f9dbcb73bb7eb4a
sureshnanjan <sureshnanjan@outlook.com>
2024-11-18 12:57:54 +0530

    Form Changes

--- csharp/GettingStarted/DemoForms/Form1.Designer.cs
+++ csharp/GettingStarted/DemoForms/Form1.Designer.cs
@@ -34,9 +34,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 57);
+            this.button1.AccessibleDescription = "This is to Invoke My Application Action";
+            this.button1.AccessibleName = "Click for More Info";
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.button1.Location = new System.Drawing.Point(436, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 65);
+            this.button1.Size = new System.Drawing.Size(249, 100);
             this.button1.TabIndex = 0;
             this.button1.Text = "CLICK ME";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,18 +48,20 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(290, 146);
+            this.textBox1.Location = new System.Drawing.Point(435, 225);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(308, 20);
+            this.textBox1.Size = new System.Drawing.Size(460, 26);
             this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "AMAZING APP";
             this.ResumeLayout(false);
