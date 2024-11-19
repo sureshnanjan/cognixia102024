f378b116a5bb729fb418b2c5774909cb5a0615c8
sureshnanjan <sureshnanjan@outlook.com>
2024-11-19 12:27:19 +0530

    First Example

--- csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
+++ csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Remote;
 namespace BrowserAutomation
 {
@@ -14,6 +15,13 @@ namespace BrowserAutomation
             driver = new RemoteWebDriver(new Uri("http://localhost:8989"), new ChromeOptions());
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Console.WriteLine(driver.Title);
+            // Click on Add Remove Link
+            driver.FindElement(By.LinkText("Add/Remove Elements")).Click();
+            // Get The Title
+            string pageTitle = driver.FindElement(By.TagName("h3")).Text;
+            // Check if it matches Add / Remove Elements
+
+            Console.WriteLine(pageTitle);
         }
 
     }
