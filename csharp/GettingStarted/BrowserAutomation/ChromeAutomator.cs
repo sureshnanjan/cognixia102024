cf2ecc0167b729fcd89b3449e9e8c82f7243dd86
sureshnanjan <sureshnanjan@outlook.com>
2024-11-19 10:48:11 +0530

    ChromeDriver Demo

--- csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
+++ csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 namespace BrowserAutomation
 {
     public class ChromeAutomator
@@ -7,7 +9,9 @@ namespace BrowserAutomation
 
         public ChromeAutomator()
         {
-            driver = new ChromeDriver();
+            //driver = new ChromeDriver();
+            IWebDriver driver;
+            driver = new RemoteWebDriver(new Uri("http://localhost:8989"), new ChromeOptions());
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Console.WriteLine(driver.Title);
         }
