c9a5eec252cadd8f8652cc3941ce8a20a68eb322
Ajay Chevuri <ajay.chevuri@ascendion.com>
2024-11-20 10:26:06 +0530

    file added

--- csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
+++ csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
@@ -1,19 +1,22 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Remote;
 namespace BrowserAutomation
 {
     public class ChromeAutomator
     {
-        ChromeDriver driver;
+        EdgeDriver driver;
 
         public ChromeAutomator()
         {
             //driver = new ChromeDriver();
             IWebDriver driver;
-            driver = new RemoteWebDriver(new Uri("http://localhost:8989"), new ChromeOptions());
+            driver = new RemoteWebDriver(new Uri("http://localhost:8666"), new EdgeOptions());
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Console.WriteLine(driver.Title);
+            var links = driver.FindElements(By.TagName("a"));
+            Console.WriteLine(links.Count);
         }
 
     }
