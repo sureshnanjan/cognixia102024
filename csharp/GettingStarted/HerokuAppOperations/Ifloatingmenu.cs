f41ad26ec91fddb72909e2b03bb0cfef537cd148
Nishanth Jaganathan <nishanth.jaganathan@ascendion.com>
2024-11-21 16:37:03 +0530

    added task

---
+++ csharp/GettingStarted/HerokuAppOperations/Ifloatingmenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface Ifloatingmenu
+    {
+        /// <summary>
+        /// Opens the target URL for testing the floating menu.
+        /// </summary>
+        /// <param name="url">The URL of the floating menu page.</param>
+
+       public void ScrollTo(int position);
+
+        /// <summary>
+        /// Checks if the floating menu is visible on the screen.
+        /// </summary>
+        /// <returns>A boolean indicating whether the floating menu is visible.</returns>
+       public bool IsFloatingMenuVisible();
+
+        /// <summary>
+        /// Closes the browser.
+        /// </summary>
+       public void CloseBrowser();
+    }
+}
---
+++ csharp/GettingStarted/HerokuAppWebdriverAdapter/floatingmenu.cs
@@ -0,0 +1,42 @@
+﻿using HerokuAppOperations;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppWebdriverAdapter
+{
+    public class floatingmenu : HerokuAppCommon, Ifloatingmenu
+
+    {
+        public void CloseBrowser()
+        {
+            
+        }
+
+        public bool IsFloatingMenuVisible()
+        {
+            try
+            {
+                // Locate the floating menu
+                IWebElement floatingMenu = driver.FindElement(By.Id("menu"));
+                // Check if the menu is displayed
+                return floatingMenu.Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+     
+
+        public void ScrollTo(int position)
+        {
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript($"window.scrollTo(0, {position});");
+        }
+    }
+}
