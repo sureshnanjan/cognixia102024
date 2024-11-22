0ae413d6702b59d33b0bd889969da6b955f282c9
Hemanth Mittapelli <hemanth.mittapelli@ascendion.com>
2024-11-22 14:18:58 +0530

    files added

--- csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
+++ csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
+using HerokuAppWebdriverAdapter;
 namespace BrowserAutomation
 {
     public class ChromeAutomator
@@ -13,28 +14,29 @@ namespace BrowserAutomation
 
         public ChromeAutomator()
         {
-            if (configfile.EndsWith("chrome"))
-            {
-                driver = new ChromeDriver();
-            }
-            if (configfile.EndsWith("firefox"))
-            {
-                driver = new FirefoxDriver();
-            }
+            //if (configfile.EndsWith("chrome"))
+            //{
+            //    driver = new ChromeDriver();
+            //}
+            //if (configfile.EndsWith("firefox"))
+            //{
+            //    driver = new FirefoxDriver();
+            //}
 
-            driver = new ChromeDriver();
+            driver = new EdgeDriver();
             //IWebDriver driver;
-            driver = new RemoteWebDriver(new Uri("http://localhost:8989"), new ChromeOptions());
+            driver = new RemoteWebDriver(new Uri("http://localhost:51389"), new EdgeOptions());
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Console.WriteLine(driver.Title);
             // Click on Add Remove Link
-            driver.FindElement(By.LinkText("Add/Remove Elements")).Click();
+            driver.FindElement(By.LinkText("Frames")).Click();
             // Get The Title
             string pageTitle = driver.FindElement(By.TagName("h3")).Text;
             // Check if it matches Add / Remove Elements
 
             Console.WriteLine(pageTitle);
             // Print the total number of links on the Home Page
+
         }
 
     }
---
+++ csharp/GettingStarted/HerokuAppOperations/IFrames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IFrames
+    {
+        
+
+        public void OnClickingNestedframes();
+        public void CheckTopframe();
+        public void CheckBottomframe();        
+        public void CheckLeftframe();
+        public void CheckRightframe();
+        public void CheckMiddleframe();
+
+
+        public void OnClickingiFrame();
+    }
+}
