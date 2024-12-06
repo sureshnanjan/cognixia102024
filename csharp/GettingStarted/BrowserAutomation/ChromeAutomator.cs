df3d0cb4b9c6c79c086245cae57f62019f6b4c33
Raj Kiran Kadambalu <raj.kadambalu@ascendion.com>
2024-12-06 11:10:25 +0530

    latest commit

--- csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
+++ csharp/GettingStarted/BrowserAutomation/ChromeAutomator.cs
@@ -1,40 +1,35 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 namespace BrowserAutomation
 {
     public class ChromeAutomator
     {
-        //ChromeDriver driver;
-        IWebDriver driver;
-        string configfile;
+        ChromeDriver driver;
 
         public ChromeAutomator()
         {
-            if (configfile.EndsWith("chrome"))
-            {
-                driver = new ChromeDriver();
-            }
-            if (configfile.EndsWith("firefox"))
-            {
-                driver = new FirefoxDriver();
-            }
-
-            driver = new ChromeDriver();
-            //IWebDriver driver;
+            //driver = new ChromeDriver();
+            IWebDriver driver;
             driver = new RemoteWebDriver(new Uri("http://localhost:8989"), new ChromeOptions());
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Console.WriteLine(driver.Title);
+            //Links Total Number
+            Console.WriteLine("Displaying the links and number of link is:");
+            var clink = driver.FindElements(By.TagName("li"));
+            foreach (var li in clink)
+            {
+                Console.WriteLine(li.Text);
+            }
+            Console.WriteLine($"Number of links on the page: {clink.Count}");
             // Click on Add Remove Link
             driver.FindElement(By.LinkText("Add/Remove Elements")).Click();
             // Get The Title
             string pageTitle = driver.FindElement(By.TagName("h3")).Text;
             // Check if it matches Add / Remove Elements
-
             Console.WriteLine(pageTitle);
-            // Print the total number of links on the Home Page
+
         }
 
     }
